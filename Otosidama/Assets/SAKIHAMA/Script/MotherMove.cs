03c2542fa0a333c5d52511f6e8064c6942087f6e
kaeru-009 <shuuto-sakihama-19h@stu.kbc.ac.jp>
2022-01-20 14:53:59 +0900

    母親のアニメーション完成

---
+++ Otosidama/Assets/SAKIHAMA/Script/MotherMove.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotherMove : MonoBehaviour
+{
+    public Animator anima;      //ドアのアニメーション
+    private float time;         //タイム変数
+    int Count = 0;              //カウント変数
+    //public bool Gflg;                  //仮フラグ
+
+    //void Start()
+    //{
+    //    //仮
+    //    Gflg = false;
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKeyDown(KeyCode.M) && Gflg == false)
+        //{
+        //    Gflg = true;
+        //}
+
+        //if (Gflg == true) { 
+            time += Time.deltaTime;
+
+            //ドアノブの処理
+            if (time > 1 && anima.GetBool("Transfer") == false)
+            {
+                //ドアノブのBoolを逆に変更
+                anima.SetBool("Turn", !anima.GetBool("Turn"));
+                //カウントアップする
+                Count++;
+                //タイムを0に戻す
+                time = 0;
+            }
+
+            //5秒間の処理
+
+            //ドアノブが3回下がったらドアが開く
+            if (Count / 2 > 2)
+            {
+                //アニメーション移行flgをtrue
+                anima.SetBool("Transfer", true);
+                //ドアのBoolを逆に変更し閉じる
+                anima.SetBool("Open", !anima.GetBool("Open"));
+                //タイムを0に戻す
+                time = 0;
+                //カウントを0に戻す
+                Count = 0;
+            }
+
+            //ドアが開き始めてからの時間を計る
+            if (anima.GetBool("Transfer") == true)
+            {
+                //ドアが開いている時間を計測
+                time += Time.deltaTime;
+            }
+
+            ////アニメーション移行flgがtrueならなおかつ1回置いて
+            if (anima.GetBool("Transfer") == true && time > 5/*Count > 1*/)
+            {
+                //ドアのBoolを逆に変更し閉じる
+                anima.SetBool("Open", !anima.GetBool("Open"));
+            }
+
+        //ドアのflgがfalseなおかつアニメーション移行flgがtrueなら入る(ドアが閉じる時の処理)
+        if (anima.GetBool("Open") == false && anima.GetBool("Transfer") == true)
+        {
+            //アニメーション移行flgをfalseにする
+            anima.SetBool("Transfer", false);
+            //タイムを0に戻す
+            time = 0;
+            //Gflg = false;
+            //mother.SetActive(false);
+        }
+        //}
+    }
+}
---
+++ Otosidama/Assets/SAKIHAMA/Script/nome.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//未使用
+
+public class nome : MonoBehaviour
+{
+    //private int Playtime = 5;    //再生時間
+    //private float time = 0;            //時間を格納
+    public Animator anima;       //母親のアニメーション
+    public bool MotherFig;     //何にも使っていない何かに使っていい奴
+    private GameObject Mother;  //ママのオブジェクト情報を格納
+    //public GameObject Door;     //ドアのオブジェクト情報を格納
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        MotherFig = false;
+        Mother = this.gameObject;
+        //仮
+        Mother.SetActive(true);
+    }
+
+    void Update()
+    {
+        //MotherFigがtrueの場合処理開始
+        if (Mother.activeSelf == true)
+        {
+            if(Input.GetKey(KeyCode.L))
+            {
+                Mother.transform.position -= new Vector3(0, 0, 0.1f);
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                anima.SetBool("Move", !anima.GetBool("Move"));
+            }
+
+            if(Input.GetKeyDown(KeyCode.Z))
+            {
+                anima.SetBool("Looking", !anima.GetBool("Looking"));
+            }
+        }
+  
+    }
+
+    ////母親処理
+    //void MotherMove()
+    //{
+
+    //}
+}
+
