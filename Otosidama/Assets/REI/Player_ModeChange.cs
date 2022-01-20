3ede4a884d02691a69c10d3b9e19dbb9b165d388
Ray_12 <rei-shimabukuro-19h@stu.kbc.ac.jp>
2022-01-20 13:22:02 +0900

    勉強と漫画読むアニメーション作成

---
+++ Otosidama/Assets/REI/Player_ModeChange.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_ModeChange : MonoBehaviour
+{
+    Animator animator;
+    public void OnClick()
+    {
+        //if (animator.ModeChange == false)
+        //{
+        //}
+    animator.SetBool("ModeChange", false);
+    }
+}
