4c9c705990bbbcabbca5754aa0d48dd429c5439b
DRQE <https://github.com/DRQE/mamimumema>
2022-01-23 14:34:14 +0900

    約２割の確率で鶏のスピードが倍になるように

--- Otosidama/Assets/Shun'sFolder/Scripts/ChickenFeint.cs
+++ Otosidama/Assets/Shun'sFolder/Scripts/ChickenFeint.cs
@@ -7,6 +7,7 @@ public class ChickenFeint : MonoBehaviour
 
     // Start is called before the first frame update
     private bool IsOnce;
+    private float randSpeed;
     void Start()
     {
         transform.position = new Vector3(3.0f, 0.3f, -2.5f);
@@ -32,15 +33,27 @@ public class ChickenFeint : MonoBehaviour
     {
         float sin = Mathf.Sin(Time.time * 5);
         
-
-        //５秒で移動z軸を4～-2まで移動
-        transform.position += new Vector3(0, (sin / 100),
-                                        (-1.2f * Time.deltaTime));
+        if(randSpeed > 7)
+        {
+            //下の２倍の速度で移動
+            transform.position += new Vector3(0, (sin / 100),
+                                (-2.4f * Time.deltaTime));
+            Debug.Log("２倍だよ");
+        }
+        else
+        {
+            //５秒で移動z軸を4～-2まで移動
+            transform.position += new Vector3(0, (sin / 100),
+                                (-1.2f * Time.deltaTime));
+            Debug.Log("通常だよ");
+        }
     }
 
     public void PosInit()
     {
         transform.position = new Vector3(3.0f, 0.3f, 4.0f);
         IsOnce = true;
+        randSpeed = 0;
+        randSpeed = Random.Range(1, 10);
     }
 }
