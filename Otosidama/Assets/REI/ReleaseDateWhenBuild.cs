ecd54af80640db2dfe552fff8e57b4f8a91dded9
Ray_12 <rei-shimabukuro-19h@stu.kbc.ac.jp>
2022-01-24 16:29:56 +0900

    ビルド日時表示スクリプト作成中

---
+++ Otosidama/Assets/REI/ReleaseDateWhenBuild.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.IO;
+using System;
+
+namespace MyProject
+{
+    /// <summary>
+    /// ビルド時の時刻を取得する
+    /// </summary>
+    public class ReleaseDateWhenBuild : MonoBehaviour
+    {
+        private static readonly string FILE_PATH = "Assets/Resources/BuildDate.txt";
+
+        [PostProcessBuild(1)]
+        public static void OnPostProcessBuild(BuildTarget target, string path)
+        {
+            using (var writer = new BinaryWriter(File.Open(FILE_PATH, FileMode.OpenOrCreate, FileAccess.Write)))
+            {
+                writer.Write(DateTime.Now.ToString("yyyy.MM.dd.HH.mm"));
+            }
+        }
+    }
+}
