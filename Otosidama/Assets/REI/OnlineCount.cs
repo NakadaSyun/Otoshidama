e2d649138823b58091cb1849b35a91a2439d7f5e
Ray_12 <rei-shimabukuro-19h@stu.kbc.ac.jp>
2022-01-27 14:27:22 +0900

    接続人数表示スクリプト

---
+++ Otosidama/Assets/REI/OnlineCount.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class OnlineCount : MonoBehaviour
+{
+    GameObject MatchMakingobj;
+
+    Matching matchingScript;
+
+    public Text textobj;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MatchMakingobj = GameObject.Find("UI_Script");
+        matchingScript = MatchMakingobj.GetComponent<Matching>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        textobj.text = "接続人数" + PhotonNetwork.CountOfPlayersInRooms;
+
+    }
+}
